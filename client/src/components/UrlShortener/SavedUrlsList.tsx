563bfb663e825cf0253e6b19999879f26ee12c60
Ned Palacios <npdoesmc@gmail.com>
2025-05-09 15:25:17 +0800

    fix: "in" -> "on"

--- client/src/components/UrlShortener/SavedUrlsList.tsx
+++ client/src/components/UrlShortener/SavedUrlsList.tsx
@@ -78,7 +78,7 @@ export default function SavedUrlsList({
         <svg className="w-5 h-5 mr-2 text-purple-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
           <path fillRule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4z" clipRule="evenodd" />
         </svg>
-        Recently shortened URLs in your device
+        Recently shortened URLs on your device
       </h2>
 
       <div className="space-y-4">
